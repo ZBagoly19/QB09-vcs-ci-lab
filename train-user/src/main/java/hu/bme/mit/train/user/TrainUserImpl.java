83549ccb6b93d9df6c1be38698c86f78fb1584ab
Bagoly Zoltán <zoltan.bagoly@gmail.com>
2020-03-30 21:55:08 +0200

    Calling the function "setJoystickPosition"

--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -3,13 +3,29 @@ package hu.bme.mit.train.user;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainUser;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrainUserImpl implements TrainUser {
 
+	Timer updateTimer = new Timer();
+	private TimerTask updateTask = new TimerTask() {
+		@Override
+		public void run() {
+			sendStickPositionToController();
+		}
+
+		private void sendStickPositionToController() {
+			controller.setJoystickPosition(joystickPosition);
+		}
+	};
+	
 	private TrainController controller;
 	private int joystickPosition;
 
 	public TrainUserImpl(TrainController controller) {
 		this.controller = controller;
+		updateTimer.schedule(updateTask, 0, 500);
 	}
 
 	@Override
